65f718cac083a5aed669c7fe70bc4015fa10ca07
stambi4a <cityzen_s@abv.bg>
2016-02-17 16:20:09 +0200

    First working variant.

---
+++ AgeOfHeroes/Engine/NumberAssigner.cs
@@ -0,0 +1,14 @@
+﻿namespace Engine
+{
+    public static class NumberAssigner
+    {
+        static int _nextNumber = 0;
+
+        public static int GetNextNumber()
+        {
+            _nextNumber = (_nextNumber + 1);
+
+            return _nextNumber;
+        }
+    }
+}
